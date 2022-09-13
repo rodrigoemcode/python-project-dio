aaba1846238109a62aa741ae8b539dd725df4a23
Rodrigo <rodrigosilva.emcode@gmail.com>
2022-09-13 10:54:49 -0300

    Estudos de Python: variaveis e constantes

---
+++ variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = "Rodrigo"
+idade = 39
+
+nome = "Leandro"
+print(nome, idade)
+
+limite_saque_diario = 1000
+print(limite_saque_diario)
+
+
+BRAZILIAN_STATES = ["MG", "BA", "AL", "AM" ]
+print(BRAZILIAN_STATES)

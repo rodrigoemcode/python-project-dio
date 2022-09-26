5db5be7495703c9710887dd48d13c3a33c91220c
Rodrigo <rodrigosilva.emcode@gmail.com>
2022-09-26 10:33:15 -0300

    Operadores Aritméticos

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,22 @@
+
+from re import X
+
+
+produto_1 = 20
+produto_2 = 10
+
+print(produto_1 + produto_2)
+print(produto_1 - produto_2)
+print(produto_1 / produto_2)
+print(produto_1 // produto_2)
+print(produto_1 * produto_2)
+print(produto_1 % produto_2)
+print(produto_1 ** produto_2)
+
+x = 10 + 5 * 4
+print(x)
+y = (10 + 5) * 4
+print(y)
+z = 10 / 2 +  25 * 2 - 100 ** 2
+print(z)
+
